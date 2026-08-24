a0ff6ec6355d989c7ce65ec6584db07cca7474f2
Roquezadas <marcelo123yt@gmail.com>
2026-08-23 21:28:10 -0400

    Consertando erros

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [tanstackStart({ server: { entry: "server" } }), react(), tailwindcss()],
+  plugins: [tailwindcss(), tanstackStart({ server: { entry: "server" } }), react(), nitro()],
   resolve: { tsconfigPaths: true },
 });
